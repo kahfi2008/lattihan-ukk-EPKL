45a83de0eb4a82aa707253883ac7e5ee5cb06d69
kahfi2008 <304seccnd@gmail.com>
2026-08-06 13:09:38 +0700

    terbaru

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PerusahaanController;
+
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
+
+// Halaman utama
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat datang di.halaman Utama E-PKL';
 });
+
+
+// Halaman tentang
+Route::get('/tentang', function () {
+    return 'Halaman ini berisi informasi tentang modul E-PKL sekolah.';
+});
+
+
+// Halaman kontak
+Route::get('/kontak', function () {
+    return 'Hubungi guru pembimbing PKL di ruang RPL.';
+});
+
+
+
+Route::prefix('perusahaan')->name('perusahaan.')->group(function () {
+ Route::get('/',[PerusahaanController::class, 'index'])->name('index');
+ Route::get('/{id}', [PerusahaanController::class, 'show'])->name('show');
+});
+
+
+
+
+
